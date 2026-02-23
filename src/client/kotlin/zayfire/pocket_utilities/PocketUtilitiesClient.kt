273db479f5588fce25cf7b923a1e91a858ccfe92
MrJeff1 <psheldig@gmail.com>
2026-02-22 20:08:59 -0500

    first commit

---
+++ src/client/kotlin/zayfire/pocket_utilities/PocketUtilitiesClient.kt
@@ -0,0 +1,9 @@
+package zayfire.pocket_utilities
+
+import net.fabricmc.api.ClientModInitializer
+
+object PocketUtilitiesClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/kotlin/zayfire/pocket_utilities/ModItems.kt
@@ -0,0 +1,27 @@
+package zayfire.pocket_utilities
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings
+import net.minecraft.item.Item
+import net.minecraft.registry.Registries
+import net.minecraft.registry.Registry
+import net.minecraft.util.Identifier
+
+object ModItems {
+
+    val POCKET_CRAFTING_TABLE: Item = register(
+        "pocket_crafting_table",
+        Item(FabricItemSettings().maxCount(1))
+    )
+
+    private fun register(name: String, item: Item): Item {
+        return Registry.register(
+            Registries.ITEM,
+            Identifier(PocketUtilities.MOD_ID, name),
+            item
+        )
+    }
+
+    fun registerAll() {
+        // Accessing the object initializes it
+    }
+}
